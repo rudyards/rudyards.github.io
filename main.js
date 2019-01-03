540bd4d33af58a12008d6b6adc503b99f4fb3083
rudyards <rudy@mightycheese.com>
2019-01-02 16:00:38 -0800

    added some javascript and skills associated with each project

--- main.js
+++ main.js
@@ -2,4 +2,30 @@ $(document).ready(() => {
     $('#myModal').on('shown.bs.modal', function () {
       $('#myInput').trigger('focus')
     })
+
+    $('#snugcircle-container').mouseover(function() {
+        $(".snugcircle-skills").show()
+    })
+
+    $('#snugcircle-container').mouseleave(function(){
+        $(".snugcircle-skills").hide()
+    })
+
+    $('#adventure-container').mouseover(function() {
+        $(".adventure-skills").show()
+    })
+
+    $('#adventure-container').mouseleave(function(){
+        $(".adventure-skills").hide()
+    })
+
+    $('#instigator-container').mouseover(function() {
+        $(".instigator-skills").show()
+    })
+
+    $('#instigator-container').mouseleave(function(){
+        $(".instigator-skills").hide()
+    })
+
+
 });
