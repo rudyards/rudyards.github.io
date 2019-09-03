926a464a7c1bf28eb3ff8571c7e1ac41ec3f0834
rudyards <rudy@mightycheese.com>
2019-09-03 11:23:39 -0400

    Redesign

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
 $(document).ready(() => {
-    $('#myModal').on('shown.bs.modal', function () {
-      $('#myInput').trigger('focus')
-    })
-
-    $('#instigator').click(function() {
-        $("#instigator-skills").slideToggle("slow")
-    })
-
-    $('#adventure').click(function() {
-        $("#adventure-skills").slideToggle("slow")
-    })
-
-    $('#snugcircle').click(function() {
-        $("#snugcircle-skills").slideToggle("slow")
-    })
-
-    $('#cuteorcuter').click(function() {
-        $("#cuteorcuter-skills").slideToggle("slow")
-    })
-
-    $('#quoridoor').click(function() {
-        $("#quoridoor-skills").slideToggle("slow")
-    })
-
-    $('#readerly').click(function() {
-        $("#readerly-skills").slideToggle("slow")
-    })
-
-    $('#taverntalk').click(function() {
-        $("#taverntalk-skills").slideToggle("slow")
-    })
-
-    $('#triviahub').click(function() {
-        $("#triviahub-skills").slideToggle("slow")
-    })
 
 });
